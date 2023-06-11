16b5ab12451c994164120fd49dac96ba7fe2a314
LeoStormer <nzeezenwa@gmail.com>
2023-06-11 02:18:42 -0400

    Removed unnecessary lines

--- src/server/Components/PunchMachine.lua
+++ src/server/Components/PunchMachine.lua
@@ -1,11 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
--- local CapsuleCollider = require(ReplicatedStorage.Modules.CapsuleCollider)
 local Component = require(ReplicatedStorage.Packages.Component)
--- local Trove = require(ReplicatedStorage.Packages.Trove)
 
--- local CollapseRagdollBindable: BindableEvent =
--- 	ReplicatedStorage.Modules.RagdollSystem.Remotes.CollapseRagdollBindable
 local DEFAULT_PUNCH_INTERVAL = 3
 
 local PunchMachine = Component.new({
@@ -13,11 +9,11 @@ local PunchMachine = Component.new({
 })
 
 function PunchMachine:Construct()
-    self.startTime = DateTime.now().UnixTimestampMillis
+	self.startTime = DateTime.now().UnixTimestampMillis
 	self.Instance:SetAttribute("StartTime", self.startTime)
 	self.punchInterval = self.Instance:GetAttribute("PunchInterval")
 	if self.punchInterval == nil then
-        self.punchInterval = DEFAULT_PUNCH_INTERVAL
+		self.punchInterval = DEFAULT_PUNCH_INTERVAL
 		self.Instance:SetAttribute("PunchInterval", DEFAULT_PUNCH_INTERVAL)
 	end
 end
