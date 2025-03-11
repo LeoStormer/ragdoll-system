80334a42822d69284a51a8df08d283d3b58e3e0c
LeoStormer <nzeezenwa@gmail.com>
2025-03-11 14:08:40 -0400

    implement avatar joint upgrade support

--- lib/TableUtils.lua
+++ lib/TableUtils.lua
@@ -12,6 +12,16 @@ function TableUtils.filter<T>(list: { T }, filterfunction: (T) -> boolean): { T 
 	return filteredlist
 end
 
+function TableUtils.find<T>(array: { T }, filterFunction: (T) -> boolean)
+	for key, value in array do
+		if filterFunction(value) then
+			return key, value
+		end
+	end
+
+	return nil
+end
+
 function TableUtils.map<T, U>(list: { T }, mapfunction: (T) -> U): { U }
 	local mappedList = table.create(#list)
 
